915dfd33467f8ae952928228ac6f54f8f2a236f1
Yashvi-saini <yashvisaini.ys@gmail.com>
2026-01-27 21:00:28 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="min-h-screen bg-white text-black text-2xl font-bold flex items-center justify-center">hello what are you doing?</div>
+    </>
+  )
+}
+
+export default App
